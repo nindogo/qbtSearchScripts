61693f09e9207d03181f3f8de241ada49f3ad894
Ni Ndogo <nindogo@gmail.com>
2025-04-27 05:09:26 +0000

    ADD uindex.org

---
+++ uindex.py
@@ -0,0 +1,107 @@
+#VERSION: 0.1
+# AUTHORS: nindogo
+# LICENSING INFORMATION
+
+import re
+from datetime import timedelta, datetime
+from html.parser import HTMLParser
+from helpers import download_file, retrieve_url
+from novaprinter import prettyPrinter
+# some other imports if necessary
+
+def de_pub_date(date_string):
+	this_date = date_string.split()
+	#this_time_delta = timedelta
+	if len(this_date) == 3:
+		#print(this_date[0])
+		if this_date[1] == "hours":
+			print(this_date[1])
+			print(0 - float(this_date[0]))
+			t1 = timedelta(hours=float(this_date[0]))
+			print(datetime.now() - t1)
+		if this_date[1] == "days":
+			t1 = timedelta(days=float(this_date[0]))
+		if this_date[1] == "weeks":
+			t1 = timedelta(weeks=float(this_date[0]))
+		if this_date[1] == "months":
+			this_date[1] = "weeks"
+			this_date[0] = str(30 * float(this_date[0]))
+			return de_pub_date(' '.join(this_date))
+		if this_date[1] == "years":
+			this_date[1] = "days"
+			this_date[0] = str(365 * float(this_date[0]))
+			return de_pub_date(' '.join(this_date))
+			
+
+class uindex(object):
+    """
+    `url`, `name`, `supported_categories` should be static variables of the engine_name class,
+     otherwise qbt won't install the plugin.
+
+    `url`: The URL of the search engine.
+    `name`: The name of the search engine, spaces and special characters are allowed here.
+    `supported_categories`: What categories are supported by the search engine and their corresponding id,
+    possible categories are ('all', 'anime', 'books', 'games', 'movies', 'music', 'pictures', 'software', 'tv').
+    """
+
+    url = 'https://uindex.org'
+    name = 'UIndex'
+    supported_categories = {
+        'all': '',
+        'anime': '&c=7',
+        'games': '&c=3',
+        'movies': '&c=1',
+        'music': '&c=4',
+        'software': '&c=5',
+        'tv': '&c=2'
+    }
+
+    def __init__(self):
+        """
+        Some initialization
+        """
+
+    def download_torrent(self, info):
+        """
+        Providing this function is optional.
+        It can however be interesting to provide your own torrent download
+        implementation in case the search engine in question does not allow
+        traditional downloads (for example, cookie-based download).
+        """
+        print(download_file(info))
+
+    # DO NOT CHANGE the name and parameters of this function
+    # This function will be the one called by nova2.py
+    def search(self, what, cat='all'):
+        """
+        Here you can do what you want to get the result from the search engine website.
+        Everytime you parse a result line, store it in a dictionary
+        and call the prettyPrint(your_dict) function.
+
+        `what` is a string with the search tokens, already escaped (e.g. "Ubuntu+Linux")
+        `cat` is the name of a search category in ('all', 'anime', 'books', 'games', 'movies', 'music', 'pictures', 'software', 'tv')
+        """
+        
+        original_find_string = r"""<tr>.*?<a href=["'](magnet.*?)["'].*?<a href=["']([^"']*?)["'][^>]*?>([^<]*?)<.*? style=['"]white-space:nowrap[^>]*?>([^<]*?)<.*?class="g"*?>([^<]*?)<.*?class="b"*?>([^<]*?)<.*?"""
+        find_string = r"""<tr>.*?<a href=["'](?P<link>magnet.*?)["'].*?<a href=["'](?P<desc_link>[^"']*?)["'][^>]*?>(?P<name>[^<]*?)<.*?class=["']sub["'][^>]*>(?P<pub_date>[^<]*)<.*? style=['"]white-space:nowrap[^>]*?>(?P<size>[^<]*?)<.*?class="g"*?>(?P<seeds>[^<]*?)<.*?class="b"*?>(?P<leech>[^<]*?)<.*?"""
+        
+        find_torrent = re.compile(find_string, flags=(re.M|re.S))
+        
+        query = self.url + "/search.php?search=" + what.replace("%20", "+") + \
+				self.supported_categories[cat] + '&sort=seeders&order=DESC'
+        page = retrieve_url(query)
+        #print(page)
+        matches = re.finditer(find_torrent, page)
+        for match in matches:
+            result_dict = {}
+            result_dict = match.groupdict()
+            result_dict["size"] = result_dict["size"].rstrip()
+            result_dict["pub_date"] = result_dict["pub_date"].rstrip()
+            result_dict["engine_url"] = self.url
+            #de_pub_date(result_dict['pub_date'])
+            prettyPrinter(result_dict)
+
+
+if __name__ == "__main__":
+    engine = uindex()
+    engine.search('ncis')
